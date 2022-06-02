3b2a1f7ba40fcd14864ef2a6e4a84b27621ba8b2
RogalXRogal <53616070+RogalXRogal@users.noreply.github.com>
2022-06-02 11:49:40 +0200

    Add files via upload

---
+++ modules/db.php
@@ -0,0 +1,4 @@
+<?php
+  $baza = mysqli_connect("localhost", "root", "", "sklep");
+  mysqli_query($baza, "SET NAMES utf8");
+?>
---
+++ modules/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pl">
+<?php
+  session_start();
+  include("db.php");
+?>
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="author" content="...">
+  <link rel="stylesheet" href="./modules/styles/index.css">
+</head>
+
+<body>
